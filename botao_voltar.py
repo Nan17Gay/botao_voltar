2d3e30f5fc8be48946465556bad40174ef6bc9bd
Nan17Gay <usuariodedougl45@gmail.com>
2025-10-02 17:41:25 -0300

    Update botao_voltar.py

--- botao_voltar.py
+++ botao_voltar.py
@@ -3,4 +3,10 @@ voltar_button = ft.IconButton(
         icon_color="YELLOW",
         tooltip="Voltar",
         on_click=lambda e: page.go("/home"),
+
+    )
+
+header = ft.Row(
+        controls=[voltar_button],
+        alignment="start"
     )
